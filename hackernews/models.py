b678b7060dd230dab15b4e0196547cfede304f8b
rguixaro <rguixaro@protonmail.ch>
2022-03-24 09:55:00 +0100

    Added model

--- hackernews/models.py
+++ hackernews/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Submission(models.Model):
+    title = models.CharField(max_length=50, default="")
+    url = models.CharField(max_length=50, default="")
+    text = models.CharField(max_length=200, default="", null=True)
