c520e24496500c1a69eaf3ca843700990d853989
rguixaro <rguixaro@protonmail.ch>
2022-04-05 17:33:48 +0200

    Updated User & Submission models

--- hackernews/models.py
+++ hackernews/models.py
@@ -4,17 +4,17 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField(max_length=15)
-    karma = models.IntegerField()
-    about = models.TextField()
+    karma = models.IntegerField(default=1)
+    about = models.TextField(default="")
     email = models.EmailField(max_length=60)
     showdead = models.BooleanField()
     noprocrast = models.BooleanField()
     maxvisit = models.CharField(max_length=16) #integerField?
     minaway = models.CharField(max_length=16)
     delay = models.CharField(max_length=16)
-    created_at = models.TextField()
+    created_at = models.TextField(default="")
 
 class Submission(models.Model):
     title = models.CharField(max_length=50, default="")
     url = models.URLField(max_length=50, default="")
-    text = models.TextField()
+    text = models.TextField(default="")
