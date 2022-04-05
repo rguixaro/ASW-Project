4100ace212e7d1255900d5bc8c374de5115306d0
rguixaro <rguixaro@protonmail.ch>
2022-04-05 17:31:07 +0200

    Added User & Submission models

--- hackernews/models.py
+++ hackernews/models.py
@@ -2,8 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class User(models.Model):
+    username = models.CharField(max_length=15)
+    karma = models.IntegerField()
+    about = models.TextField()
+    email = models.EmailField(max_length=60)
+    showdead = models.BooleanField()
+    noprocrast = models.BooleanField()
+    maxvisit = models.CharField(max_length=16) #integerField?
+    minaway = models.CharField(max_length=16)
+    delay = models.CharField(max_length=16)
+    created_at = models.TextField()
 
 class Submission(models.Model):
     title = models.CharField(max_length=50, default="")
-    url = models.CharField(max_length=50, default="")
-    text = models.CharField(max_length=200, default="", null=True)
+    url = models.URLField(max_length=50, default="")
+    text = models.TextField()
