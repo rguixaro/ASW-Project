8b324e6b930298979ba60d7ba02819adf1991354
xavier.pina <xavier.pina@estudiantat.upc.edu>
2022-04-20 23:35:45 +0200

    implementació de la vista Subission i es links

--- hackernews/urls.py
+++ hackernews/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
     path('<username>/threads', views.threads, name='threads'),
     path('<username>/upvoted', views.upvotedSubmissions, name='upvotedSubmissions'),
     path('<username>/upvoted&comments', views.upvotedComments, name='upvotedComments'),
-    path('item/<submission_id>', views.detailedSubmission, name='detailedSubmission'),
+    path('item?id=<submission_id>', views.detailedSubmission, name='detailedSubmission'),
 
 ]
