663ff63fc40dd53817ae5861645360b948821de4
rguixaro <rguixaro@protonmail.ch>
2022-04-20 20:15:56 +0200

    static root

--- ASWProject/settings.py
+++ ASWProject/settings.py
@@ -130,6 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,  'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
