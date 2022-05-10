8f48e1cc967fe15c6e6f61a291d55d6054a8b463
ferranjrr <ferrandelavargaantoja@gmail.com>
2022-05-10 21:35:28 +0200

    Changes swagger and starting in api.py

--- hackernews/api.py
+++ hackernews/api.py
@@ -10,7 +10,13 @@ def newsUser(request, username):
 def user(request, username):
     u = User.objects.get(authUser__username=username);
     return JsonResponse(model_to_dict(u), safe=False)
-    
+
 def detailedSubmission(request, submission_id):
-    submission = Submission.objects.get(id=submission_id)
-    return JsonResponse(model_to_dict(submission), safe=False)
+    if request.method == 'GET':
+        submission = Submission.objects.get(id=submission_id)
+        return JsonResponse(model_to_dict(submission), safe=False)
+    else if request.method == 'POST':
+        comment = Comment(author=request.user, submission=submission, text=request.POST['text'])
+        comment.save()
+        return JsonResponse(model_to_dict(comment), safe=False)
+
