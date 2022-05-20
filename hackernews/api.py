cd75ce97257f99136aa8da81e94649d17dadab7b
pauantonio <pauantoniosoler@gmail.com>
2022-05-20 18:00:17 +0200

    api completed

--- hackernews/api.py
+++ hackernews/api.py
@@ -1,10 +1,15 @@
 import json
+from django.db.models import Count
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
 from hackernews.models import Submission, User, Comment, Action
 from datetime import date, datetime, timezone
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authtoken.models import Token
 
+
+def getUserByToken(token):
+    return Token.objects.get(key=token).user
 
 def newsUser(request, username):
     if not User.objects.filter(authUser__username=username).exists():
@@ -16,43 +21,56 @@ def newsUser(request, username):
     submissions = list(Submission.objects.filter(author__authUser__username=username).values())
     return JsonResponse(submissions, safe=False)
 
-def submission(request, title, url, text):
-    if request.method == 'POST':
-        author = User.objects.get(id=request.user.id)
-        if url != "":
-            if Submission.objects.filter(url=url).exists():
-                return JsonResponse({
-                    "status": 400,
-                    "error": "Bad Request",
-                    "message": "A submission with the same url was already posted"
-                }, status=404)
-            else:
-                newSubmission = Submission(title=title, url=url, author=author)
-                newSubmission.save()
-                if text != "":
-                    newComment = Comment(author=author, submission=newSubmission, text=text)
-                    newComment.save()
-        elif text != "":
-            newSubmission = Submission(title=title, text=text, type="ask", author=author)
+@csrf_exempt
+def submit(request):
+    token = request.GET.get('token')
+    username = getUserByToken(token).username
+    author = User.objects.get(authUser__username=username)
+    title = request.GET.get('title')
+    url = request.GET.get('url')
+    text = request.GET.get('text')
+    if url != "":
+        if Submission.objects.filter(url=url).exists():
+            return JsonResponse({
+                "status": 400,
+                "error": "Bad Request",
+                "message": "A submission with the same url was already posted"
+            }, status=404)
+        else:
+            newSubmission = Submission(title=title, url=url, author=author)
             newSubmission.save()
+            if text != "":
+                newComment = Comment(author=author, submission=newSubmission, text=text)
+                newComment.save()
+    elif text != "":
+        newSubmission = Submission(title=title, text=text, type="ask", author=author)
+        newSubmission.save()
 
-        return JsonResponse(newSubmission, safe=False)
+    return JsonResponse(model_to_dict(newSubmission), safe=False)
 
-    elif request.method == 'GET':
-        submissions = list(Submission.objects.values())
-        return JsonResponse(submissions, safe=False)
+def news(request):
+    submissions = list(Submission.objects.values().annotate(count=Count('upvotes')).order_by('-count'))
+    return JsonResponse(submissions, safe=False)
+
+def newest(request):
+    submissions = list(Submission.objects.values().order_by('-posted_at_date', '-posted_at_time'))
+    return JsonResponse(submissions, safe=False)
+
+def ask(request):
+    submissions = list(Submission.objects.values().filter(type="ask"))
+    return JsonResponse(submissions, safe=False)
 
 @csrf_exempt
 def user(request, username):
-    u = User.objects.get(authUser__username=username);
+    u = User.objects.get(authUser__username=username)
     if(request.method == 'GET'):
         print("get")
         return JsonResponse(model_to_dict(u), safe=False)
     else:
         updatedUser = request.body.decode('utf-8')
         body = json.loads(updatedUser)
-        u.about = body['about'];
-        u.showdead = body['showDead'];
+        u.about = body['about']
+        u.showdead = body['showDead']
         u.noprocrast = body['noprocrast']
         u.maxvisit = body['maxvisit']
         u.minaway = body['minaway']
@@ -92,7 +110,9 @@ def dateSubmissions(request, date):
 
 @csrf_exempt
 def upvoteSubmission(request, submission_id):
-    user = User.objects.get(authUser__username="pau")  # falta implementar token --> user
+    token = request.GET.get('token')
+    username = getUserByToken(token).username
+    user = User.objects.get(authUser__username=username)
     s = Submission.objects.get(id=submission_id)
     if s is None:
         return JsonResponse({
@@ -109,16 +129,152 @@ def upvoteSubmission(request, submission_id):
         "message": "Exists a user's upvote to this submission"
     }, status=404)
 
-def upvotedUser(request, username):
+@csrf_exempt
+def unvoteSubmission(request, submission_id):
+    token = request.GET.get('token')
+    username = getUserByToken(token).username
+    user = User.objects.get(authUser__username=username)
+    s = Submission.objects.get(id=submission_id)
+    if s is None:
+        return JsonResponse({
+            "status": 404,
+            "error": "Not Found",
+            "message": "No Submission with that ID"
+        }, status=404)
+    if s.upvotes.filter(action_type=Action.UPVOTE_SUBMISSION, user=user).exists():
+        s.upvotes.filter(action_type=Action.UPVOTE_SUBMISSION, user=user).delete()
+        return JsonResponse({"status": "success"}, safe=False)
+    return JsonResponse({
+        "status": 404,
+        "error": "Doesn't exists",
+        "message": "Doesn't exists a user's upvote to this submission"
+    }, status=404)
+
+def upvotedSubmissions(request):
+    token = request.GET.get('token')
+    username = getUserByToken(token).username
     user = User.objects.get(authUser__username=username)
     upvoted = list(Action.objects.filter(user=user, action_type=Action.UPVOTE_SUBMISSION).values())
+    return JsonResponse(upvoted, safe=False)
+
+@csrf_exempt
+def upvoteComment(request, comment_id):
+    token = request.GET.get('token')
+    username = getUserByToken(token).username
+    user = User.objects.get(authUser__username=username)
+    c = Comment.objects.get(id=comment_id)
+    if c is None:
+        return JsonResponse({
+            "status": 404,
+            "error": "Not Found",
+            "message": "No Comment with that ID"
+        }, status=404)
+    if not c.upvotes.filter(action_type=Action.UPVOTE_COMMENT, user=user).exists():
+        c.upvotes.create(action_type=Action.UPVOTE_COMMENT, user=user)
+        return JsonResponse({"status": "success"}, safe=False)
+    return JsonResponse({
+        "status": 404,
+        "error": "Already exists",
+        "message": "Exists a user's upvote to this comment"
+    }, status=404)
+
+@csrf_exempt
+def unvoteComment(request, comment_id):
+    token = request.GET.get('token')
+    username = getUserByToken(token).username
+    user = User.objects.get(authUser__username=username)
+    c = Comment.objects.get(id=comment_id)
+    if c is None:
+        return JsonResponse({
+            "status": 404,
+            "error": "Not Found",
+            "message": "No Comment with that ID"
+        }, status=404)
+    if c.upvotes.filter(action_type=Action.UPVOTE_COMMENT, user=user).exists():
+        c.upvotes.filter(action_type=Action.UPVOTE_COMMENT, user=user).delete()
+        return JsonResponse({"status": "success"}, safe=False)
+    return JsonResponse({
+        "status": 404,
+        "error": "Doesn't exists",
+        "message": "Doesn't exists a user's upvote to this comment"
+    }, status=404)
+
+def upvotedComments(request):
+    token = request.GET.get('token')
+    username = getUserByToken(token).username
+    user = User.objects.get(authUser__username=username)
+    upvoted = list(Action.objects.filter(user=user, action_type=Action.UPVOTE_COMMENT).values())
     return JsonResponse(upvoted, safe=False)
 
 def commentsUser(request, username):
     comments = list(Comment.objects.filter(author__authUser__username=username).values())
     return JsonResponse(comments, safe=False)
 
-def commentSubmission(request, submission_id):
-    s = Submission.object.get(id=submission_id)
-    comments = list(Comment.objects.get(submission=s).values())
+def commentsSubmission(request, submission_id):
+    s = Submission.objects.get(id=submission_id)
+    comments = list(Comment.objects.filter(submission=s).values())
     return JsonResponse(comments, safe=False)
+
+@csrf_exempt
+def commentSubmission(request, submission_id):
+    text = request.GET.get('text')
+    if text is None:
+        return JsonResponse({
+            "status": 400,
+            "error": "No Text",
+            "message": "Need text to comment"
+        }, status=400)
+    s = Submission.objects.get(id=submission_id)
+    if s is None:
+        return JsonResponse({
+            "status": 404,
+            "error": "Not Found",
+            "message": "No Submission with that ID"
+        }, status=404)
+    token = request.GET.get('token')
+    username = getUserByToken(token).username
+    author = User.objects.get(authUser__username=username)
+    newComment = Comment(text=text, author=author, submission=s)
+    newComment.save()
+    return JsonResponse({"status": "success"}, safe=False)
+
+@csrf_exempt
+def replyComment(request, comment_id):
+    text = request.GET.get('text')
+    if text is None:
+        return JsonResponse({
+            "status": 400,
+            "error": "No Text",
+            "message": "Need text to comment"
+        }, status=400)
+    c = Comment.objects.get(id=comment_id)
+    if c is None:
+        return JsonResponse({
+            "status": 404,
+            "error": "Not Found",
+            "message": "No Comment with that ID"
+        }, status=404)
+    token = request.GET.get('token')
+    username = getUserByToken(token).username
+    author = User.objects.get(authUser__username=username)
+    submission_id = c.submission.id
+    s = Submission.objects.get(id=submission_id)
+    newComment = Comment(text=text, author=author, submission=s, parent=c)
+    newComment.save()
+    return JsonResponse({"status": "success"}, safe=False)
+
+@csrf_exempt
+def editProfile(request):
+    about = request.GET.get('about')
+    if about is None:
+        return JsonResponse({
+            "status": 400,
+            "error": "No Text",
+            "message": "Need text to update about"
+        }, status=400)
+    token = request.GET.get('token')
+    username = getUserByToken(token).username
+    user = User.objects.get(authUser__username=username)
+    user.about = about
+    user.save()
+    return JsonResponse({"status": "success"}, safe=False)
